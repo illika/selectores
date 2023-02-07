bc6066b92b740f3980ee9202be00907eb6816f2d
illika <steve.milan7@gmail.com>
2023-02-06 22:28:40 -0500

    Servicio Pais

--- src/app/paises/pages/selector-page/selector-page.component.ts
+++ src/app/paises/pages/selector-page/selector-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Pais } from '../../interfaces/pais.interface';
 import { PaisesService } from '../../services/paises.service';
 
 @Component({
@@ -13,18 +14,27 @@ export class SelectorPageComponent implements OnInit {
   constructor(private fb: FormBuilder, private paisesService: PaisesService) { }
 
   miFormulario: FormGroup = this.fb.group({
-    region: ["", [Validators.required]]
+    region: ["", [Validators.required]],
+    pais: ["", [Validators.required]]
   });
 
   regiones: string[] = [];
+  paises: Pais[] = [];
 
   ngOnInit(): void {
     this.regiones = this.paisesService.regiones;
+
+    this.miFormulario.get("region")?.valueChanges
+      .subscribe(
+        (region) => {
+          if (region === "") return;
+          this.paisesService.paisesByRegion(region)
+            .subscribe((pais) => this.paises = pais);
+        });
   }
 
   guardar() {
     console.log(this.miFormulario);
-
   }
 
 }
--- src/app/paises/services/paises.service.ts
+++ src/app/paises/services/paises.service.ts
@@ -1,29 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Pais } from '../interfaces/pais.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaisesService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   private _regiones: string[] = [
-    "EU",
-    "EFTA",
-    "CARICOM",
-    "PA",
-    "AU",
-    "USAN",
-    "EEU",
-    "AL",
-    "ASEAN",
-    "CAIS",
-    "CEFTA",
-    "NAFTA",
-    "SAARC"
+    "EU", "EFTA", "CARICOM", "PA", "AU", "USAN", "EEU",
+    "AL","ASEAN", "CAIS", "CEFTA","NAFTA","SAARC"
   ];
+
+  private baseUrl : string = "https://restcountries.com/v2";
 
   get regiones() {
     return [...this._regiones];
+  }
+
+  paisesByRegion(region: string) : Observable<Pais[]>{
+    const url = `${this.baseUrl}/regionalbloc/${region}?fields=alpha3Code,name`;
+    return this.http.get<Pais[]>(url);
   }
 }
